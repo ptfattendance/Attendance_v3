583ddf6d42713bae03cbea0ce3d4ade7331c13ba
Deepak <deepakck.ptf@gmail.com>
2023-12-31 21:11:18 +0530

    change sts in reset password

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -333,7 +333,7 @@ exports.resetPassword = async (req, res) => {
         await user.save();
 
         // Return a success message
-       return  res.json({ message: 'Password reset successfully' });
+        return res.status(200).json({message:"Password resetted successfully"});
     } catch (err) {
         console.error(err);
        return  res.status(500).json({ message: err.message });
